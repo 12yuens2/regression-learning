cbd1ea7a2c19ca98f0252c9d515ba32e3363720c
12yuens2 <sizhe1007@gmail.com>
2018-09-12 10:04:58 +0100

    Update src images and notebooks

--- src/script.py
+++ src/script.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-
+'''
 def get_linreg(intercept, normalise):
     return LinearRegression(fit_intercept=intercept, normalize=normalise, n_jobs=-1)
 
@@ -33,24 +33,16 @@ def polyreg_predict(X, y, test_input, degrees, intercept=False, normalise=True):
     linreg.fit(x_hat, y)
 
     return linreg.predict(predict_hat)
+'''
 
-
-EnergySet = pd.read_csv("data/energydata_complete.csv")
-df = pd.DataFrame(data=EnergySet)
-
-output = df["Appliances"]
-input = df.loc[0:, "lights":"RH_9"]
-test_input = df.loc[:1000, "lights":"RH_9"]
-poly_degrees = 3
+# Get data
 
 
 
 
-correlation_matrix = df.corr()
+from visualisation import *
 
-#print(correlation_matrix["Appliances"].sort_values(ascending=False))
-
-print(df["T_out"].describe())
+plot_histograms(df)
 
 
 
@@ -60,7 +52,6 @@ for i in range(2, len(df.columns)):
     fig = ax.get_figure()
     fig.savefig("plots/" + str(df.columns[i]) + "-plot.png")
 '''
-
 
 '''
 print(input.shape)
